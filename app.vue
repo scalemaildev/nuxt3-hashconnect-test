f64b93a501cd60c8b0f68dbc6d599c2b0fb023b5
scalemaildev <scale@scalemail.com>
2022-06-29 21:39:52 -0500

    init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup="setup" lang="ts">
+import { HashConnect } from 'hashconnect';
+
+console.log(HashConnect);
+</script>
